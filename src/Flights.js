d994f600cf292cca9b5d555ffda3741c63f5eae4
Jeffrey Ernst <Jeffrey@yachtcloud.eu>
2019-12-10 11:13:44 +0100

    ddd

--- src/Flights.js
+++ src/Flights.js
@@ -1,7 +1,30 @@
+import Screen from './Screen'
+
 export default class Flights {
-  static async getFlights () {
+  constructor () {
+    this.screen = new Screen()
+  }
+
+  async getFlights () {
     const data = await fetch('/flights.json')
     const { flights } = await data.json()
     return flights
+  }
+
+  async handleFlights (blob) {
+    const input = blob.target.value
+    this.screen.clear()
+    if (input.length < 3) return
+
+    const flights = await this.getFlights()
+
+    flights
+      .filter(fl => fl.airport.toLowerCase().includes(input.toLowerCase()))
+      .sort((a, b) => {
+        if (a.score > b.score) return -1
+        if (a.score < b.score) return 1
+        return 0
+      })
+      .forEach(fl => { this.screen.addFlight(fl) })
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import '../sass/index.scss'
 import Flights from './Flights'
-import Screen from './Screen'
 
-const screen = new Screen()
+const flights = new Flights()
 
 document.querySelector('#flight-input')
-  .addEventListener('input', async (blob) => {
-      const input = blob.target.value
-      screen.clear()
-      if (input.length < 3) return
-
-      const flights = await Flights.getFlights()
-      flights
-        .filter(flight => flight.airport.toLowerCase().includes(input.toLowerCase()))
-        .sort((a, b) => {
-            if (a.score > b.score) return -1
-            if (a.score < b.score) return 1
-            return 0
-        })
-        .forEach(flight => { screen.addFlight(flight) })
-  })
-
-  Flights.getFlights()
+  .addEventListener('input', function (input) { flights.handleFlights(input) })
